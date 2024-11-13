8c2f1ea51365e22a2f0ac394303731bfc8849e1b
JKS <javier.kuri@anahuac.mx>
2024-11-13 15:33:20 -0600

    Commit 2 13/11/2024
    Creados archivos template, carrito, catalogo, cerrar, hostorial, iniciar, y index, con todos basandose en el archivo template.
    Navegación entre archivos mediante top navigation bar.

---
+++ PHP/carrito.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-sm bg-primary navbar-light fixed-top">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="../index.html">
+                <img src="../IMG/control.png" alt="LOGO" style="width:40px;" class="rounded-pill"> 
+            </a>
+            <div class="container-fluid">
+                <a class="navbar-text h1 text-decoration-none" href="../index.html">TIENDA DE JUEGOS</a>
+            </div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="catalogo.php">Catalogo de productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="historial.php">Historial de compras</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="carrito.php">Carrito</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="iniciar.php">Iniciar sesión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cerrar.php">Cerrar sesión</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
